99dafe0f7f69f11d3098deec52d575051511327f
Ali Hasan Kazmi <alihasankazmi72@gmail.com>
2025-09-09 00:00:39 +0530

    Added more array problems

--- src/b_arrayInJava/smallestElement.java
+++ src/b_arrayInJava/smallestElement.java
@@ -2,16 +2,30 @@ package b_arrayInJava;
 
 public class smallestElement {
     public static void main(String[] args) {
-        int[] arr = {3,5,6,10,9};
+        int[] arr = {3,5,3,10,9};
         int smallest = arr[0];
         for (int i = 0; i < arr.length -1; i++) {
             int temp;
             if (arr[i]<smallest){
-                temp = smallest;
-                smallest=arr[i];
-                arr[i]=temp;
+//            smallest = arr[i];
+            smallest = Math.min(smallest,arr[i]);
             }
         }
         System.out.println("Smallest Element: "+smallest);
     }
 }
+
+//for swapping
+
+//        int a =6;
+//        int b =5;
+//         a = a+b;
+//         b = a-b;
+//         a = a-b;
+//        System.out.println(a + " " +b);
+
+//                using third variable
+//                temp = smallest;
+//                smallest=arr[i];
+//                arr[i]=temp;
+//                wihtout using third variable
