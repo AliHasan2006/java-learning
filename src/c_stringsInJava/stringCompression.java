dad91ad68e5cc2a4b3785095d83f1005135740f0
AliHasan@2006 <alihasankazmi72@gmail.com>
2026-01-11 00:37:13 +0530

    String Compression Problem

---
+++ src/c_stringsInJava/stringCompression.java
@@ -0,0 +1,33 @@
+package c_stringsInJava;
+
+public class stringCompression {
+    public static void main(String[] args) {
+        char[] arr = {'a','a','b','c','c','c','c'};
+        int idx = 0;
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            char ch = arr[i];
+            int count = 0;
+
+            while (i < n && ch == arr[i]) {
+                count++;
+                i++;
+            }
+
+            arr[idx++] = ch;
+
+            if (count > 1) {
+                String str = Integer.toString(count);
+                for (char dig : str.toCharArray()) {
+                    arr[idx++] = dig;
+                }
+            }
+
+            i--;
+        }
+
+        // ✅ Correct output
+        System.out.println(new String(arr, 0, idx));
+    }
+}
