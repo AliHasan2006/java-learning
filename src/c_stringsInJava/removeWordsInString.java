bb1194561abb3bea394aa8e55ce15c4e28b0c8d0
AliHasan@2006 <alihasankazmi72@gmail.com>
2026-02-04 10:24:09 +0530

    Solved String Compressio problem

--- src/c_stringsInJava/removeWordsInString.java
+++ src/c_stringsInJava/removeWordsInString.java
@@ -2,29 +2,31 @@ package c_stringsInJava;
 
 public class removeWordsInString {
     public static void main(String[] args) {
-        StringBuilder str = new StringBuilder("the pen");
+        StringBuilder str = new StringBuilder("kon ho tum");
         System.out.println("Orignal String: "+str);
         str.reverse(); // reverse entire string
 
-        String ans = "";
+        StringBuilder ansString = new StringBuilder();
         int n = str.length();
 
         for (int i = 0; i < n; i++) {
-            StringBuilder word = new StringBuilder("");
+            StringBuilder word = new StringBuilder();
 
             while (i < n && str.charAt(i) != ' ') {
                 word.append(str.charAt(i));
                 i++;
             }
 
-            if (word.length() != 0) {
+            if (!word.isEmpty()) {
                 word.reverse();          // reverse each word
-                ans += word + " ";       // append instead of overwrite
+                ansString.append(word).append(' ');   // append instead of overwrite
             }
         }
 
-        System.out.println("Reversed Words String: "+ans.trim());
+        ansString.toString();
+        System.out.println("Reversed Words String: "+ansString);
+        System.out.println(str.length());
+        System.out.println(ansString.length());
         System.out.println("Your string reversed successfully with: \nTime Complexity: O(n)\nSpace COmplexity: O(n)");
-
     }
 }
